66e4518fc8a9ad7ce14c5ccfde65c9ac466447fa
Patryk Ptasiński <patryk@ipepe.pl>
2025-03-26 02:58:48 +0100

    Implemented menu system with dynamic display and selection features in M5Reader.ino.

--- M5Reader.ino
+++ M5Reader.ino
@@ -3,6 +3,38 @@
 
 M5GFX display;
 
+// Menu constants
+static constexpr int MENU_COLS = 2;
+static constexpr int MENU_ROWS = 4;
+static constexpr int MENU_ITEMS = MENU_COLS * MENU_ROWS;
+
+struct MenuItem {
+  const char* label;
+  void (*action)();
+};
+
+// Forward declarations
+void menuAction1();
+void menuAction2();
+void menuAction3();
+void menuAction4();
+void menuAction5();
+void menuAction6();
+void menuAction7();
+void menuAction8();
+
+MenuItem menuItems[MENU_ITEMS] = {
+  {"Books", menuAction1},
+  {"Settings", menuAction2},
+  {"Recent", menuAction3},
+  {"Search", menuAction4},
+  {"Bookmarks", menuAction5},
+  {"Calibrate", menuAction6},
+  {"About", menuAction7},
+  {"Help", menuAction8},
+};
+
+int selectedMenuItem = 0;
 
 void displaySplashScreen(void)
 {
@@ -20,6 +52,55 @@ void displaySplashScreen(void)
     display.println("M5Reader");
 }
 
+void displayMenu() {
+  display.clear(TFT_WHITE);
+  
+  int displayWidth = display.width();
+  int displayHeight = display.height();
+  
+  int tileWidth = displayWidth / MENU_COLS;
+  int tileHeight = displayHeight / MENU_ROWS;
+  
+  display.setTextSize(2);
+  
+  for (int i = 0; i < MENU_ITEMS; i++) {
+    int col = i % MENU_COLS;
+    int row = i / MENU_COLS;
+    
+    int x = col * tileWidth;
+    int y = row * tileHeight;
+    
+    // Draw tile border
+    if (i == selectedMenuItem) {
+      display.fillRect(x, y, tileWidth, tileHeight, TFT_BLACK);
+      display.setTextColor(TFT_WHITE, TFT_BLACK);
+    } else {
+      display.drawRect(x, y, tileWidth, tileHeight, TFT_BLACK);
+      display.setTextColor(TFT_BLACK, TFT_WHITE);
+    }
+    
+    // Center text in tile
+    int textWidth = strlen(menuItems[i].label) * 12; // Approximate width
+    int textX = x + (tileWidth - textWidth) / 2;
+    int textY = y + (tileHeight - 16) / 2; // 16 is approximate text height
+    
+    display.setCursor(textX, textY);
+    display.print(menuItems[i].label);
+  }
+  
+  display.display();
+}
+
+// Placeholder menu actions
+void menuAction1() { /* Books functionality */ }
+void menuAction2() { /* Settings functionality */ }
+void menuAction3() { /* Recent functionality */ }
+void menuAction4() { /* Search functionality */ }
+void menuAction5() { /* Bookmarks functionality */ }
+void menuAction6() { /* Calibrate functionality */ }
+void menuAction7() { /* About functionality */ }
+void menuAction8() { /* Help functionality */ }
+
 void setup(void)
 {
     display.begin();
@@ -27,9 +108,12 @@ void setup(void)
     display.invertDisplay(true);
     display.clear(TFT_WHITE);
     displaySplashScreen();
+    delay(2000); // Show splash screen for 2 seconds
+    displayMenu();
 }
 
 void loop(void)
 {
-    delay(1000);
+    // Handle touch input (to be implemented)
+    delay(100);
 }
